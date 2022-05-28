901751cc9c30cc5b2f90689196200942d66b20a0
Elias Neto <eliasneto321@outlook.com>
2022-05-28 04:15:13 -0300

    primeiro-commit

---
+++ Assets/categoria-masculino/gola-polo/gola-polo-basica-penteada/gola-polo-basica-penteada.js
@@ -0,0 +1,164 @@
+function changeColor1() {
+  document
+    .querySelector('.img-product')
+    .classList.remove(
+      'changeColor1',
+      'changeColor2',
+      'changeColor3',
+      'changeColor4',
+      'changeColor5',
+      'changeColor6',
+      'changeColor7',
+    )
+  document.querySelector('.img-product').classList.add('changeColor1')
+
+  document.querySelector('.s2').classList.remove('focus')
+  document.querySelector('.s3').classList.remove('focus')
+  document.querySelector('.s4').classList.remove('focus')
+  document.querySelector('.s5').classList.remove('focus')
+  document.querySelector('.s6').classList.remove('focus')
+  document.querySelector('.s7').classList.remove('focus')
+  document.querySelector('.s1').classList.add('focus')
+}
+
+function changeColor2() {
+  document
+    .querySelector('.img-product')
+    .classList.remove(
+      'changeColor1',
+      'changeColor2',
+      'changeColor3',
+      'changeColor4',
+      'changeColor5',
+      'changeColor6',
+      'changeColor7',  
+    )
+  document.querySelector('.img-product').classList.add('changeColor2')
+
+  document.querySelector('.s1').classList.remove('focus')
+  document.querySelector('.s3').classList.remove('focus')
+  document.querySelector('.s4').classList.remove('focus')
+  document.querySelector('.s5').classList.remove('focus')
+  document.querySelector('.s6').classList.remove('focus')
+  document.querySelector('.s7').classList.remove('focus')
+  document.querySelector('.s2').classList.add('focus')
+}
+
+function changeColor3() {
+  document
+    .querySelector('.img-product')
+    .classList.remove(
+      'changeColor1',
+      'changeColor2',
+      'changeColor3',
+      'changeColor4',
+      'changeColor5',
+      'changeColor6',
+      'changeColor7',  
+    )
+  document.querySelector('.img-product').classList.add('changeColor3')
+
+  document.querySelector('.s1').classList.remove('focus')
+  document.querySelector('.s2').classList.remove('focus')
+  document.querySelector('.s4').classList.remove('focus')
+  document.querySelector('.s5').classList.remove('focus')
+  document.querySelector('.s6').classList.remove('focus')
+  document.querySelector('.s7').classList.remove('focus')
+  document.querySelector('.s3').classList.add('focus')
+}
+
+function changeColor4() {
+  document
+    .querySelector('.img-product')
+    .classList.remove(
+      'changeColor1',
+      'changeColor2',
+      'changeColor3',
+      'changeColor4',
+      'changeColor5',
+      'changeColor6',
+      'changeColor7',   
+    )
+  document.querySelector('.img-product').classList.add('changeColor4')
+
+  document.querySelector('.s1').classList.remove('focus')
+  document.querySelector('.s2').classList.remove('focus')
+  document.querySelector('.s3').classList.remove('focus')
+  document.querySelector('.s5').classList.remove('focus')
+  document.querySelector('.s6').classList.remove('focus')
+  document.querySelector('.s7').classList.remove('focus')
+  document.querySelector('.s8').classList.remove('focus')
+  document.querySelector('.s4').classList.add('focus')
+}
+
+function changeColor5() {
+  document
+    .querySelector('.img-product')
+    .classList.remove(
+      'changeColor1',
+      'changeColor2',
+      'changeColor3',
+      'changeColor4',
+      'changeColor5',
+      'changeColor6',
+      'changeColor7', 
+    )
+  document.querySelector('.img-product').classList.add('changeColor5')
+
+  document.querySelector('.s1').classList.remove('focus')
+  document.querySelector('.s2').classList.remove('focus')
+  document.querySelector('.s3').classList.remove('focus')
+  document.querySelector('.s4').classList.remove('focus')
+  document.querySelector('.s6').classList.remove('focus')
+  document.querySelector('.s7').classList.remove('focus')
+  document.querySelector('.s8').classList.remove('focus')
+  document.querySelector('.s5').classList.add('focus')
+}
+
+function changeColor6() {
+  document
+    .querySelector('.img-product')
+    .classList.remove(
+      'changeColor1',
+      'changeColor2',
+      'changeColor3',
+      'changeColor4',
+      'changeColor5',
+      'changeColor6',
+      'changeColor7',
+    )
+  document.querySelector('.img-product').classList.add('changeColor6')
+
+  document.querySelector('.s1').classList.remove('focus')
+  document.querySelector('.s2').classList.remove('focus')
+  document.querySelector('.s3').classList.remove('focus')
+  document.querySelector('.s4').classList.remove('focus')
+  document.querySelector('.s5').classList.remove('focus')
+  document.querySelector('.s7').classList.remove('focus')
+  document.querySelector('.s8').classList.remove('focus')
+  document.querySelector('.s6').classList.add('focus')
+}
+
+function changeColor7() {
+  document
+    .querySelector('.img-product')
+    .classList.remove(
+      'changeColor1',
+      'changeColor2',
+      'changeColor3',
+      'changeColor4',
+      'changeColor5',
+      'changeColor6',
+      'changeColor7',
+    )
+  document.querySelector('.img-product').classList.add('changeColor7')
+
+  document.querySelector('.s1').classList.remove('focus')
+  document.querySelector('.s2').classList.remove('focus')
+  document.querySelector('.s3').classList.remove('focus')
+  document.querySelector('.s4').classList.remove('focus')
+  document.querySelector('.s5').classList.remove('focus')
+  document.querySelector('.s6').classList.remove('focus')
+  document.querySelector('.s8').classList.remove('focus')
+  document.querySelector('.s7').classList.add('focus')
+}
